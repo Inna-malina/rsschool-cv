36825ccfdce445c429ccd0f4f9fccdb23280e351
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-03-18 13:52:35 +0700

    feat: add file for translating

---
+++ js/translate.js
@@ -0,0 +1,34 @@
+const translate = {
+    'en': {
+        'name': 'Cherkashina Inna Aleksandrovna',
+        'about': 'About me',
+        'skills': 'My skills',
+        'portfolio': 'Portfolio',
+        'experience': 'My experience',
+        'contacts': 'Contacts',
+        'about-description-1': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'about-description-2': 'Voluptatum inventore reprehenderit vero ratione quis laudantium voluptatibus iusto, cupiditate assumenda quos?',
+        'about-description-3': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'about-description-4': 'Voluptatum inventore reprehenderit vero ratione quis laudantium voluptatibus iusto, cupiditate assumenda quos?',
+        'about-description-5': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+        'about-description-6': 'Voluptatum inventore reprehenderit vero ratione quis laudantium voluptatibus iusto, cupiditate assumenda quos?'
+    },
+    'ru': {
+        'name': 'Черкашина Инна Александровна',
+        'about': 'Обо мне',
+        'skills': 'Мои навыки',
+        'portfolio': 'Портфолио',
+        'experience': 'Мой опыт',
+        'contacts': 'Контакты',
+        'about-description-1': 'Люблю всё новое, постоянно учусь. Одно время сильно увлекалась фотографией и работой с различными фото редакторами, видео редакторами даже закончила несколько курсов по этим темам.',
+        'about-description-2': 'Люблю шить одежду. Вела курсы по шитью и выигрывала всевозможные конкурсы.',
+        'about-description-3': 'Очень люблю музыку. Когда-то давно играла на фортепиано, даже закончила музыкальное училище и успела проработать в этой сфере 7 лет.',
+        'about-description-4': 'Вам может показаться, что всё перечисленное мной вообще никак не связано между собой, а тем более с веб разработкой, но это только на первый взгляд.',
+        'about-description-5': 'Музыкальное образование дало развитие логики, памяти. Пошив одежды - умение продумывать и конструировать сложные детали, делать расчёты, а так же понимание цвета. Увлечение фотографией - понимание композиции, какая картинка нравится людям, а какая картинка вызовет отторжение.',
+        'about-description-6': 'А ещё я люблю эзотерику и астральные путешествия.'
+    }
+};
+
+
+
+export default translate;
