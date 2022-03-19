2896efeb8da55f551cc9578a97d04a51d148926d
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-03-19 09:04:44 +0700

    feat: modified css, index.html, translate

--- js/translate.js
+++ js/translate.js
@@ -6,12 +6,15 @@ const translate = {
         'portfolio': 'Portfolio',
         'experience': 'My experience',
         'contacts': 'Contacts',
-        'about-description-1': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-        'about-description-2': 'Voluptatum inventore reprehenderit vero ratione quis laudantium voluptatibus iusto, cupiditate assumenda quos?',
-        'about-description-3': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-        'about-description-4': 'Voluptatum inventore reprehenderit vero ratione quis laudantium voluptatibus iusto, cupiditate assumenda quos?',
-        'about-description-5': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-        'about-description-6': 'Voluptatum inventore reprehenderit vero ratione quis laudantium voluptatibus iusto, cupiditate assumenda quos?'
+        'about-description-1': 'I love everything new, I am constantly learning. At one time I started studying photography and working with various photo editors, video editors and even completed several courses on these topics.',
+        'about-description-2': 'I love sewing clothes. I taught sewing courses and won all sorts of competitions.',
+        'about-description-3': 'I love music very much. Once upon a time I played the piano, graduated from a music school and worked in this area for 7 years.',
+        'about-description-4': 'It may seem to you that everything I have listed has nothing to do with each other, and even more so with web development, but this is only at first glance.',
+        'about-description-5': 'Musical education gave the development of logic, memory. Tailoring - the ability to think through and design complex details, make calculations, as well as an understanding of color. Passion for photography - understanding the composition, which picture people like, and which picture will cause rejection.',
+        'about-description-6': 'I also love esotericism and astral travellings.',
+        'skills-description-1': 'Also I can do dynamic layout and adaptive layout for different sizes of screens.',
+        'skills-description-2': 'I use flexboxes, greeds, animation and etc.',
+        'skills-other': 'My other skills'
     },
     'ru': {
         'name': 'Черкашина Инна Александровна',
@@ -25,7 +28,10 @@ const translate = {
         'about-description-3': 'Очень люблю музыку. Когда-то давно играла на фортепиано, даже закончила музыкальное училище и успела проработать в этой сфере 7 лет.',
         'about-description-4': 'Вам может показаться, что всё перечисленное мной вообще никак не связано между собой, а тем более с веб разработкой, но это только на первый взгляд.',
         'about-description-5': 'Музыкальное образование дало развитие логики, памяти. Пошив одежды - умение продумывать и конструировать сложные детали, делать расчёты, а так же понимание цвета. Увлечение фотографией - понимание композиции, какая картинка нравится людям, а какая картинка вызовет отторжение.',
-        'about-description-6': 'А ещё я люблю эзотерику и астральные путешествия.'
+        'about-description-6': 'А ещё я люблю эзотерику и астральные путешествия.',
+        'skills-description-1': 'Так же я могу делать динамическую и адаптивную вёрстку для разных размеров экранов',
+        'skills-description-2': 'Я использую флексбоксы, гриды, анимацию и т.д.',
+        'skills-other': 'Мои дополнительные навыки'
     }
 };
 
