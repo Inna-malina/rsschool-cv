aeb77fdecc32cb0db2541af43f58427c909fe52d
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-05-10 11:31:40 +0700

    feat: change text in index.html and translate.js in about-description-2

--- js/translate.js
+++ js/translate.js
@@ -8,7 +8,7 @@ const translate = {
         'contacts': 'Contacts',
 
         'about-description-1': 'Hi. My name is Inna Cherkashina.',
-        'about-description-2': 'My dream is a work as a web developer in a large IT company.',
+        'about-description-2': 'My dream is a making useful IT projects, applications, sites for people.',
         'about-description-3': 'I have proven track record in creating websites for two building company and also an advertising agency.',
         'about-description-4': 'Working knowleges HTML, CSS, JS and attention to detail for bug-free website.',
         'about-description-5': 'I have a good comunication skills and a personalized approach to clients needs.',
@@ -28,7 +28,7 @@ const translate = {
         'contacts': 'Контакты',
 
         'about-description-1': 'Привет. Меня зовут Инна',
-        'about-description-2': 'Моя мечта - работа в большой IT компании',
+        'about-description-2': 'Мечтаю делать полезные IT проекты, приложения или сайты для людей.',
         'about-description-3': 'Я имею успешный опыт работы в создании сайтов для двух строительных компаний, а так же для рекламного агентства.',
         'about-description-4': 'Имею рабочие знания HTML, CSS, JS и внимание к деталям для создания работочего сайта без багов и ошибок.',
         'about-description-5': 'У меня довольно неплохие коммуникативные навыки и умение находить персональный подход к каждому клиенту',
